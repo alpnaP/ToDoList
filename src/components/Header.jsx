00af5ebfc58bf662e639a0f8169f963106a9e619
Patel <1996alpanapatel@gmail.com>
2024-12-17 11:10:42 +0530

    todolist24

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+export default function Header() {
+  return (
+
+    <header>
+      <h1>
+        <HighlightIcon />
+        ToDoList
+      </h1>
+    </header>
+  );
+
+
+
+}
